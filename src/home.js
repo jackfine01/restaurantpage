569d24d84bfa7c317e05bce86a93cc743548173e
jack <jackfine01@gmail.com>
2025-01-24 13:24:24 -0500

    adding createHomePage and exporting it to index.js

--- src/home.js
+++ src/home.js
@@ -1,1 +1,13 @@
 import { clearBoard, createHeader, createSubHeader, createItem } from "./base.js";
+
+function createHomePage(){
+    clearBoard();
+
+    createHeader('Home')
+    createSubHeader('About')
+
+    createItem('Hours', 'Monday-Sunday: 7am-9pm')
+    createItem('About', 
+        'Sample Resturaunt is the best resturaunt this side of the Lorem Ipsum!  Come visit us at 12345 Rd')
+};
+export { createHomePage };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 console.log("I am live")
 import "./styles.css";
-import { clearBoard, createHeader, createSubHeader, createItem } from "./base.js";
+// import { clearBoard, createHeader, createSubHeader, createItem } from "./base.js";
+import { createHomePage } from "./home.js";
 
-createHeader('SampleTitle');
-createSubHeader('SubSampleTitle')
-createItem('Item', 'She lorem till I ipsum')
+// createHeader('SampleTitle');
+// createSubHeader('SubSampleTitle')
+// createItem('Item', 'She lorem till I ipsum')
+createHomePage();
