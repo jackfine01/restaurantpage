a8203fafe637309e97877d55e267eb7f76943bad
jack <jackfine01@gmail.com>
2025-01-24 12:36:35 -0500

    finishing layout and styling.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 console.log("I am live")
 import "./styles.css";
+
+function clearBoard(){
+    // find all p, delete them
+    // find all .items, delete them
+    // find all .subheaders, delete them
+    // find all .headers, delete them
+};
