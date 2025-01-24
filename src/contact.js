7ccc755010bb69752aeda1ec3c8ca896b8aea32f
jack <jackfine01@gmail.com>
2025-01-24 13:31:41 -0500

    adding contact page creation.

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,13 @@
 import { clearBoard, createHeader, createSubHeader, createItem } from "./base.js";
+
+function createContactPage(){
+    clearBoard();
+
+    createHeader('Contact')
+    createSubHeader('Contact Information')
+
+    createItem('Jefe Maximo', 'Phone: 111-222-333  Email: JefeMax@sample.com')
+    createItem('General Contactman', 'Phone: 444-555-666  Email: gencon@sample.com')
+    createItem('Sampler Supreme', 'Phone: 777-888-999  Email: samsupreme@sample.com')
+};
+export { createContactPage };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ console.log("I am live")
 import "./styles.css";
 // import { clearBoard, createHeader, createSubHeader, createItem } from "./base.js";
 import { createHomePage } from "./home.js";
-
+import { createContactPage } from "./contact.js";
 // createHeader('SampleTitle');
 // createSubHeader('SubSampleTitle')
 // createItem('Item', 'She lorem till I ipsum')
-createHomePage();
+createContactPage();
